76eb7dad28da6126f385c37ce4c739907220b2fc
Shantanu Gote <shantanugote82@gmail.com>
2026-06-28 10:44:20 +0530

    chore: remove footer from inside app dashboards

--- app/src/app/(main)/layout.tsx
+++ app/src/app/(main)/layout.tsx
@@ -2,7 +2,6 @@
 
 import { Sidebar } from "@/components/sidebar";
 import { TopBar } from "@/components/topbar";
-import { SiteFooter } from "@/components/site-footer";
 import { SearchProvider } from "@/lib/search-context";
 import { ClinicProvider } from "@/lib/clinic-context";
 import { SessionProvider } from "@/lib/session-context";
@@ -21,7 +20,6 @@ export default function MainLayout({
           <Sidebar />
           <div className="flex-1 flex flex-col min-w-0 md:pl-56 pb-14 md:pb-0">
             <main className="flex-1">{children}</main>
-            <SiteFooter />
           </div>
         </div>
         </SessionProvider>
